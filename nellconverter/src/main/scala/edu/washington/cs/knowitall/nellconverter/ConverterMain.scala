7038e0dd2ffb70606ac6bb917b130e00703efb88
David H Jung <davejung@uw>
2012-08-21 18:55:22 -0700

    Fixed a bug, added methods for clarity.

--- nellconverter/src/main/scala/edu/washington/cs/knowitall/nellconverter/ConverterMain.scala
+++ nellconverter/src/main/scala/edu/washington/cs/knowitall/nellconverter/ConverterMain.scala
@@ -21,6 +21,9 @@ object ConverterMain extends App {
     relationMap += (relation(0).toLowerCase -> (relation(1), relation(2)))
   }
   
+  // TODO: process the file with nell patterns used by extractors to get a 
+  // mapping from relation to some collection of patterns. 
+  
   for (line <- io.Source.fromFile("NELL_relations_620.csv").getLines.map(line => line.split("\t"))
       if !isFirstLine(line)) {
     val arg1 = getEntityString(line)
@@ -28,33 +31,38 @@ object ConverterMain extends App {
     val arg2 = getValueString(line)
     println(arg1 + " " + rel + " " + arg2)
   }
- 
+  
+  /** Determines whether line is the first line in the nell beliefs file.
+   *  @param line a line from the nell beliefs file.
+   *  @return true if first line; false otherwise.
+   */
   def isFirstLine(line: Array[String]): Boolean = {
     line(Relation) == "Relation"
   }
   
-  // gets the "best entity literal string" from a line from the belief file
-  def getEntityString(line: Array[String]): String = {
-    val length = line.length
-    // index of entity string will be: 
-    //   length - 2 (if no categories), 
-    //   length - 3 (if one category), or 
-    //   length - 4 (if two categories) 
-    if (!line(length - 1).startsWith("concept:")) line(length - 2)
-    else if (!line(length - 2).startsWith("concept:")) line(length - 3)
-    else line(length - 3)
+  /** Gets the "Best Entity literalString" from a line from the belief file.
+   *  @param a line from the nell beliefs file
+   *  @return line's "Best Entity literalString" value.  
+   */  def getEntityString(line: Array[String]): String = {
+    getEVString(line, 'entity)
   }
   
-  // gets the "best value literal string" from a line from the belief file
+  /** Gets the "Best Value literalString" from a line from the belief file.
+   *  @param a line from the nell beliefs file
+   *  @return line's "Best Value literalString" value.  
+   */
   def getValueString(line: Array[String]): String = {
-    val length = line.length
-    // index of value string will be: 
-    //   length - 2 (if no categories), 
-    //   length - 3 (if one category), or 
-    //   length - 4 (if two categories) 
-    if (!line(length - 1).startsWith("concept:")) line(length - 1)
-    else if (!line(length - 2).startsWith("concept:")) line(length - 2)
-    else line(length - 3)
+    getEVString(line, 'value)
+  }
+  
+  // method that gets either the value string or the entity string
+  def getEVString(line: Array[String], x: Symbol): String = {
+    // offset is how far from the end of the line to go to find the entity/value.
+    val offset = if (x == 'entity) 1 else 0
+    
+    if (hasTwoCategories(line)) line(line.length - 3 - offset)
+    else if (hasOneCategory(line)) line(line.length - 2 - offset)
+    else line(line.length - 1 - offset)
   }
   
   // gets the literal string representation of a relation from a line from the belief file
@@ -67,4 +75,32 @@ object ConverterMain extends App {
     relationPair.getOrElse(throw new IllegalArgumentException())._1
   }
   
+  /** Determines whether a line from the beliefs file has both 
+   *  a value for "Categories for Entity" and "Categories for Value"
+   *  @param line a line from the nell beliefs file.
+   *  @return true if line contains exactly two categories; false otherwise
+   */
+  def hasTwoCategories(line: Array[String]): Boolean = {
+    val length = line.length
+    line(length - 1).startsWith("concept:") && line(length - 2).startsWith("concept:")
+  } 
+  
+  /** Determines whether a line from the beliefs file only has 
+   *  a value for "Categories for Entity" or "Categories for Value".
+   *  @param line a line from the nell beliefs file.
+   *  @return true if line contains exactly one category; false otherwise
+   */
+  def hasOneCategory(line: Array[String]): Boolean = {
+    val length = line.length
+    line(length - 1).startsWith("concept:") && !line(length - 2).startsWith("concept:")
+  }
+  
+  /** Determines whether a line from the beliefs file has no categories. 
+   *  @param line a line from the nell beliefs file.
+   *  @return true if line contains no categories; false otherwise. 
+   */
+  def hasNoCategories(line: Array[String]): Boolean = {
+    val length = line.length
+    !line(length - 1).startsWith("concept:") && !line(length - 2).startsWith("concept:")
+  }
 }
